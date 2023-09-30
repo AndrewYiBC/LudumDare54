e69bbc8b8a3d3c1ba008a4f8ee6af128638fed50
Yutong Yi <YiAndrew2001@gmail.com>
2023-09-30 16:32:13 -0400

    Updated viewing UI

--- LudumDare54/Assets/Scripts/ViewCanvas.cs
+++ LudumDare54/Assets/Scripts/ViewCanvas.cs
@@ -11,6 +11,12 @@ public class ViewCanvas : MonoBehaviour
     // Variables
     [SerializeField] private TextMeshProUGUI xValue;
     [SerializeField] private TextMeshProUGUI yValue;
+    [SerializeField] private GameObject xLimitText;
+    [SerializeField] private GameObject yLimitText;
+    private float xLimit = 38.95f;
+    private float yLimit = 18.95f;
+    private bool is_xLimit = false;
+    private bool is_yLimit = false;
 
     void Start()
     {
@@ -21,5 +27,37 @@ public class ViewCanvas : MonoBehaviour
     {
         xValue.text = (transform.position.x * 10f).ToString("F1") + " meters";
         yValue.text = (transform.position.y * 10f).ToString("F1") + " meters";
+        if (transform.position.x > xLimit || transform.position.x < -xLimit)
+        {
+            if (!is_xLimit)
+            {
+                is_xLimit = true;
+                xLimitText.SetActive(true);
+            }
+        }
+        else
+        {
+            if (is_xLimit)
+            {
+                is_xLimit = false;
+                xLimitText.SetActive(false);
+            }
+        }
+        if (transform.position.y > yLimit || transform.position.y < -yLimit)
+        {
+            if (!is_yLimit)
+            {
+                is_yLimit = true;
+                yLimitText.SetActive(true);
+            }
+        }
+        else
+        {
+            if (is_yLimit)
+            {
+                is_yLimit = false;
+                yLimitText.SetActive(false);
+            }
+        }
     }
 }
