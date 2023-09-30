1798daa8ea67bc7bfefb76784d440d9894ada251
nanx2 <123844948+nanx2@users.noreply.github.com>
2023-09-30 12:40:52 -0700

    Clicking on a sprite prints to console

--- LudumDare54/Assets/Scripts/SpriteButton.cs
+++ LudumDare54/Assets/Scripts/SpriteButton.cs
@@ -2,31 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class SpriteButton : MonoBehaviour
 {
-    Rigidbody2D rb;
-    [SerializeField] float speed = 7f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-
-        rb.velocity = new Vector2(inputX * speed, 0);
+        
     }
 
     void OnEnable()
     {
-        GameObject[] otherObjects = GameObject.FindGameObjectsWithTag("Activity");
+        GameObject[] otherObjects = GameObject.FindGameObjectsWithTag("Player");
 
         foreach (GameObject obj in otherObjects)
         {
             Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), collision.collider);
     }
 }
