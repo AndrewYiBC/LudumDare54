71c529a78f73358da0e914c2427b1576c823218e
Yutong Yi <YiAndrew2001@gmail.com>
2023-09-30 17:41:36 -0400

    Basic grappling hook

---
+++ LudumDare54/Assets/Scripts/RetrieveTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetrieveTarget : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private GameObject grapplePoint;
+    [SerializeField] private GameObject grapple;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
