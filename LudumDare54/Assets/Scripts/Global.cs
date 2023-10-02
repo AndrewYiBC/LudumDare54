8b4327a5f37b5b496d375dc775e047e1d5bcd19d
Yutong Yi <YiAndrew2001@gmail.com>
2023-10-02 15:46:22 -0400

    Updated submarine items

---
+++ LudumDare54/Assets/Scripts/Global.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Global
+{
+    public static int periscopeCount = 0;
+    public static int portholeCount = 0;
+    public static int itemCount = 0;
+    public static bool pipeFixed = false;
+    public static bool wallFixed = false;
+    public static bool gaugeFixed = false;
+}
